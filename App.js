bb718340180e4b627354d923a944d2cf2b55c69f
Erick <libertti@live.com>
2021-02-24 10:26:19 -0300

    Aprendendo React

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+
+class App extends Component{
+  render(){
+    return (
+      <View>
+        <Text>Olá Mundo!</Text>
+        <Text>Meu Primeiro App</Text>
+      </View>
+
+
+    );
+
+
+  }
+
+}
+
+export default App;
+
+
+
+/* function app(){
+  return(
+    <View>
+      <Text>Olá Mundo!</Text>
+    </View>
+  );
+}
+
+export default app();
+*/
