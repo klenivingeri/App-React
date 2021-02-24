fbf6dbb70c740e66de05efdadaf57714230a87a0
Erick <libertti@live.com>
2021-02-24 10:41:13 -0300

    Entendendo Props

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View, Text,Image} from 'react-native';
 
 
 class App extends Component{
   render(){
+    let nome  = 'Steve Jobs';
+    let img  = 'https://sujeitoprogramador.com/steve.png';
     return (
       <View>
         <Text>Olá Mundo!</Text>
         <Text>Meu Primeiro App</Text>
+        <Text style={{fontSize: 25,margin:15}} >
+          {nome}
+        </Text>
+        <Image
+          source={{uri: img }}
+          style={{width: 300, height: 300}}
+        />
       </View>
-
-
     );
-
-
   }
-
 }
 
 export default App;
