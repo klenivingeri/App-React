80917c4b06f04d2e90717b43e54546d75c78e059
Erick <libertti@live.com>
2021-03-01 22:33:04 -0300

    Aprendendo sobre propriedade flex css

--- App.js
+++ App.js
@@ -7,12 +7,12 @@ class App extends Component{
 
   render(){
     return(
-      <View style={styles.area}>
+      <View style={{flex:1,backgroundColor:'#222'}}>
 
-        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Eu sou texto 1</Text>
-        <Text style={styles.alinhaTexto}>Eu sou texto 2</Text>
-        <Text>Eu sou texto 3</Text>
-        <Text style={styles.textoPrincipal}>Eu sou texto 4</Text>
+          <View style={ {height:65,  backgroundColor:'red'} } />
+          <View style={ {flex:1,  backgroundColor:'green'} } />
+          <View style={ {height:65,  backgroundColor:'yellow'} } />
+  
 
       </View>
     );
@@ -20,82 +20,6 @@ class App extends Component{
 
 }
 
-
-const styles = StyleSheet.create({
-  area:{
-    margin: 100
-  },
-  textoPrincipal:{
-    fontSize:25,
-    color: '#ff0000'
-  },
-  alinhaTexto:{
-   textAlign: 'center', 
-  },
-})
 
 export default App;
 
-
-
-
-
-
-/*
-
-class App extends Component{
-  render(){
-    let nome  = 'Steve Jobs';
-    let texto = 'Steven Paul Jobs foi um inventor, empresário e magnata americano no setor da informática.'
-    return (
-      <View>
-        
-        <Text>Olá Mundo!</Text>
-        <Text>Meu Primeiro App</Text>
-        <Text style={{fontSize: 25,margin:15}} >
-          {nome}
-        </Text>
-        <Jobs largura={500} altura={200} texto={texto} />
-      </View>
-    );
-  }
-}
-
-
-class Jobs extends Component{
-  render(){
-    let img  = 'https://sujeitoprogramador.com/steve.png';
-
-    return(
-      <View>
-        <Image 
-          source={{uri: img}}
-          style={{width: this.props.largura, height: this.props.altura}}
-          />
-        <Text style={{fontSize: 9.6}}> 
-        {this.props.texto}
-        </Text>
-      </View>
-    );
-  }
-}
-
-export default App;
-
-
-*/
-
-
-
-
-
-/* function app(){
-  return(
-    <View>
-      <Text>Olá Mundo!</Text>
-    </View>
-  );
-}
-
-export default app();
-*/
